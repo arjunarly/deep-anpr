5565a9fdfd2a03c284443772989cc4b9135cc4c5
AndyWang <synckey@gmail.com>
2016-07-17 22:11:09 +0800

    change output layer node count

--- train.py
+++ train.py
@@ -233,6 +233,8 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
                             (last_batch_idx - batch_idx))
                         last_batch_idx = batch_idx
                         last_batch_time = batch_time
+                        last_weights = [p.eval() for p in params]
+                        numpy.savez("weights.npz", *last_weights)
 
         except KeyboardInterrupt:
             last_weights = [p.eval() for p in params]
